767193d37f95257464babdaa447f543d2777af1d
Luis Manuel Rodriguez Scio PC <lwe_@hotmail.com>
2016-05-16 17:04:13 -0500

    Create ASP.NET MVC project

---
+++ GoogleCalendarAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoogleCalendarAPI.Startup))]
+namespace GoogleCalendarAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
